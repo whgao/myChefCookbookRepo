4d1c28eaf2b59dca7d3ac8da8527b02e0210c700
wenhui <wenhui.gao2@partnet.samsung.com>
2013-08-08 19:54:31 +0000

    Added the about_me cookbook

---
+++ cookbooks/about_me/attributes/me.rb
@@ -0,0 +1,5 @@
+
+set[:about_me][:first_name] = "Wenhui"
+default[:about_me][:favorite][:food] = "pizza"
+default[:about_me][:favorite][:drink] = "water"
+default[:about_me][:favorite][:hobby] = "running"
