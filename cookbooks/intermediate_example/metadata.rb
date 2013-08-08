049bceb620ab2dc8dcae77fc6ada2e39ff32c580
wenhui <wenhui.gao2@partnet.samsung.com>
2013-08-08 00:12:37 +0000

    Added a hello world message to the default.rb recipe

---
+++ cookbooks/intermediate_example/metadata.rb
@@ -0,0 +1,10 @@
+name             'intermediate_example'
+maintainer       'YOUR_COMPANY_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures intermediate_example'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+recipe "intermediate_example::default",
+       "Prints hello would output"
---
+++ cookbooks/intermediate_example/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: intermediate_example
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+log "Hello World! You should try Chef with RightScale!"
+
