9f3ce0e606dff2b0ca507c307f1d79f85aa17b83
wenhui <wenhui.gao2@partnet.samsung.com>
2013-08-08 01:53:30 +0000

    Added rightscale_marker to default.rb

--- cookbooks/intermediate_example/metadata.rb
+++ cookbooks/intermediate_example/metadata.rb
@@ -6,5 +6,7 @@ description      'Installs/Configures intermediate_example'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+depends "rightscale"
+
 recipe "intermediate_example::default",
        "Prints hello would output"
